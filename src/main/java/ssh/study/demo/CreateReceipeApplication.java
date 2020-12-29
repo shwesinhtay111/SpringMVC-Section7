5822beb5c3861d0b2ad08bc65ecfc225dc97456e
ShweSinHtay <shwesin@isgm2.com>
2020-12-29 10:26:44 +0630

    first commit

---
+++ src/main/java/ssh/study/demo/CreateReceipeApplication.java
@@ -0,0 +1,13 @@
+package ssh.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateReceipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateReceipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ssh/study/demo/CreateReceipeApplicationTests.java
@@ -0,0 +1,13 @@
+package ssh.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateReceipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
